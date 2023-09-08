0c5f6d5bd82e6e903aa548f8ba876228806bf7eb
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-09-08 04:37:46 -0700

    fix contact page

--- src/content/LogIn/Register.jsx
+++ src/content/LogIn/Register.jsx
@@ -9,14 +9,23 @@ function Register() {
       <h1 className="text-4xl font-semibold">Register</h1>
       <p className="font-medium text-base mt-9">Email Address</p>
       <form>
-        <Input placeholder={"Enter your mail address"} />{" "}
-        <p className="mt-9 text-base max-w-[453px]">A link to set a new password will be sent to your email address.</p>
-        <p className="mt-4 text-base max-w-[453px]">
-          Your personal data will be used to support your experience throughout
-          this website, to manage access to your account, and for other purposes
-          described in our <b>privacy policy.</b>
-        </p>
-        <button type="submit" className="py-5 px-20 border mt-[64px] hover:bg-primary hover:border-primary hover:text-white border-black rounded-xl">Register</button>
+        <Input placeholder={"Enter your mail address"} />
+        <div>
+          <p className="mt-9 text-base max-w-[453px]">
+            A link to set a new password will be sent to your email address.
+          </p>
+          <p className="mt-4 text-base max-w-[453px]">
+            Your personal data will be used to support your experience
+            throughout this website, to manage access to your account, and for
+            other purposes described in our <b>privacy policy.</b>
+          </p>
+        </div>
+        <button
+          type="submit"
+          className="py-5 px-20 border mt-[64px] hover:bg-primary hover:border-primary hover:text-white border-black rounded-xl"
+        >
+          Register
+        </button>
       </form>
     </div>
   );
