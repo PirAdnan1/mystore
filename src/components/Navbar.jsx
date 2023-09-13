f177e774878d75cb1c783697a1f13cc390cca74f
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-09-13 03:51:17 -0700

    fix navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,103 +26,111 @@ export default function Navbar() {
 
   return (
     <div className="bg-special">
-    <Container>
-      <div className="flex justify-between items-center  py-7 px-4">
-        <Link href="/">
-          <Logo />
-        </Link>
-        <button
-          onClick={() => setIsMenuOpen(true)}
-          className={clsx(
-            "pb-4 text-primary transition-colors hover:text-cool-grey-800",
-            {
-              hidden: !isMobile,
-            }
-          )}
-        >
-          <Burger className="text-2xl" />
-        </button>
-        <div
-          className={clsx("flex flex-grow transition-transform duration-500", {
-            "fixed inset-0 z-50 translate-x-[100%] bg-white": isMobile,
-            "!translate-x-0": isMenuOpen,
-          })}
-        >
-          <div
-            className={clsx({
-              "flex flex-grow items-center justify-between": !isMobile,
-
-              "mx-auto flex max-w-sm flex-grow flex-col gap-12 text-2xl":
-                isMobile,
-            })}
-          >
-            <div
-              className={clsx("flex h-[100px] items-end justify-between", {
+      <Container>
+        <div className="flex justify-between items-center  py-7 px-4">
+          <Link href="/">
+            <Logo />
+          </Link>
+          <div className="flex gap-4">
+            <div className="lg:hidden">
+              <AddtoCard />
+            </div>
+            <button
+              onClick={() => setIsMenuOpen(true)}
+              className={clsx("pb-4 text-primary", {
                 hidden: !isMobile,
               })}
             >
-              <Logo className="text-xs pl-2" />
-              <button
-                onClick={() => setIsMenuOpen(false)}
-                className="pb-4 pr-4 text-primary transition-colors hover:text-cool-grey-800"
-              >
-                <Close />
-              </button>
-            </div>
-            <ul
-              className={clsx(
-                "flex items-center 2xl:space-x-12 lg:gap-16 gap-10 lg:ml-auto",
-                {
-                  "flex-col": isMobile,
-                }
-              )}
-            >
-              <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link href="/" className="hover:text-primary text-base">
-                  Home
-                </Link>
-              </li>
-              <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link href="/shop" className="hover:text-primary text-base">
-                  Shop
-                </Link>
-              </li>
-              <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link href="#" className="hover:text-primary text-base">
-                  About
-                </Link>
-              </li>
-              <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link href="/contact" className="hover:text-primary text-base">
-                  Contact
-                </Link>
-              </li>
-              <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link href="#" className="hover:text-primary text-base">
-                  Tools
-                </Link>
-              </li>
-            </ul>
+              <Burger className="text-2xl" />
+            </button>
+          </div>
+          <div
+            className={clsx(
+              "flex flex-grow transition-transform duration-500",
+              {
+                "fixed inset-0 z-50 translate-x-[100%] bg-white": isMobile,
+                "!translate-x-0": isMenuOpen,
+              }
+            )}
+          >
             <div
-              className={clsx("flex justify-center gap-4 lg:ml-auto", {
-                "w-full mx-auto  max-w-sm flex-col": isMobile,
+              className={clsx({
+                "flex flex-grow items-center justify-between": !isMobile,
+
+                "mx-auto flex max-w-sm flex-grow flex-col gap-12 text-2xl":
+                  isMobile,
               })}
             >
-              <div className="flex space-x-12 ml-28">
-                <Link href="/account">
-                  <User />
-                </Link>
-                <Search />
-                <LoveIcon />
-                <Link href="/cart">
-                  <AddtoCard />{" "}
-                </Link>
+              <div
+                className={clsx("flex h-[100px] items-end justify-between", {
+                  hidden: !isMobile,
+                })}
+              >
+                <Logo className="text-xs pl-2" />
+                <button
+                  onClick={() => setIsMenuOpen(false)}
+                  className="pb-4 pr-4 text-primary transition-colors hover:text-cool-grey-800"
+                >
+                  <Close />
+                </button>
+              </div>
+              <ul
+                className={clsx(
+                  "flex items-center 2xl:space-x-12 xl:gap-16 gap-16 md:gap-7 lg:ml-auto",
+                  {
+                    "flex-col": isMobile,
+                  }
+                )}
+              >
+                <li className="hover:text-primary text-xs text-secondary font-bold">
+                  <Link href="/" className="hover:text-primary text-base">
+                    Home
+                  </Link>
+                </li>
+                <li className="hover:text-primary text-xs text-secondary font-bold">
+                  <Link href="/shop" className="hover:text-primary text-base">
+                    Shop
+                  </Link>
+                </li>
+                <li className="hover:text-primary text-xs text-secondary font-bold">
+                  <Link href="#" className="hover:text-primary text-base">
+                    About
+                  </Link>
+                </li>
+                <li className="hover:text-primary text-xs text-secondary font-bold">
+                  <Link
+                    href="/contact"
+                    className="hover:text-primary text-base"
+                  >
+                    Contact
+                  </Link>
+                </li>
+                <li className="hover:text-primary text-xs text-secondary font-bold">
+                  <Link href="#" className="hover:text-primary text-base">
+                    Tools
+                  </Link>
+                </li>
+              </ul>
+              <div
+                className={clsx("flex justify-center gap-4 lg:ml-auto", {
+                  "w-full mx-auto  max-w-sm flex-col": isMobile,
+                })}
+              >
+                <div className="flex space-x-12 justify-center">
+                  <Link href="/account">
+                    <User />
+                  </Link>
+                  <Search />
+                  <LoveIcon />
+                  <Link href="/cart" className="hidden lg:block">
+                    <AddtoCard />{" "}
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
     </div>
   );
 }
