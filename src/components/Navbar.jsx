078ffdd6550952297ef7e677889f4a28dbc32125
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-09-13 03:32:12 -0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 // hooks
 import { useState } from "react";
-import { useRouter } from "next/router";
 
 // util
 import clsx from "clsx";
-import cn from "@/utils/cn";
 
 // components
 import Container from "./Container";
@@ -25,10 +23,11 @@ import AddtoCard from "@/assets/AddtoCard";
 export default function Navbar() {
   const isMobile = useIsMobile(1024);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const router = useRouter();
+
   return (
+    <div className="bg-special">
     <Container>
-      <div className="flex justify-between items-center bg-special py-7 px-4 lg:px-10 shadow-md">
+      <div className="flex justify-between items-center  py-7 px-4">
         <Link href="/">
           <Logo />
         </Link>
@@ -71,47 +70,35 @@ export default function Navbar() {
               </button>
             </div>
             <ul
-              className={clsx("flex items-center 2xl:space-x-12 lg:gap-16 gap-10 lg:ml-auto", {
-                "flex-col": isMobile,
-              })}
+              className={clsx(
+                "flex items-center 2xl:space-x-12 lg:gap-16 gap-10 lg:ml-auto",
+                {
+                  "flex-col": isMobile,
+                }
+              )}
             >
               <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link
-                  href="/"
-                  className="hover:text-primary text-base"
-                >
+                <Link href="/" className="hover:text-primary text-base">
                   Home
                 </Link>
               </li>
               <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link
-                  href="/shop"
-                  className="hover:text-primary text-base"
-                >
+                <Link href="/shop" className="hover:text-primary text-base">
                   Shop
                 </Link>
               </li>
               <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link
-                  href="#"
-                  className="hover:text-primary text-base"
-                >
+                <Link href="#" className="hover:text-primary text-base">
                   About
                 </Link>
               </li>
               <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link
-                  href="/contact"
-                  className="hover:text-primary text-base"
-                >
+                <Link href="/contact" className="hover:text-primary text-base">
                   Contact
                 </Link>
               </li>
               <li className="hover:text-primary text-xs text-secondary font-bold">
-                <Link
-                  href="#"
-                  className="hover:text-primary text-base"
-                >
+                <Link href="#" className="hover:text-primary text-base">
                   Tools
                 </Link>
               </li>
@@ -136,108 +123,6 @@ export default function Navbar() {
         </div>
       </div>
     </Container>
+    </div>
   );
 }
-
-// import React, { useState } from "react";
-// import Link from "next/link";
-
-// // components
-// import Container from "./Container";
-
-// // assets
-// import Logo from "@/assets/Logo";
-// import User from "@/assets/User";
-// import Search from "@/assets/Search";
-// import LoveIcon from "@/assets/LoveIcon";
-// import AddtoCard from "@/assets/AddtoCard";
-
-// function Navbar() {
-//   const [show, setShow] = useState(false);
-
-//   const handleChange = () => {
-//     setShow(!show);
-//   };
-
-//   return (
-//     <div className="bg-special text-base font-semibold w-full h-full">
-//       <Container>
-//         <div className="bg-special  flex items-center ">
-//           <Link href="/">
-//             <Logo />
-//           </Link>
-//           <ul className="flex space-x-12 ">
-//             <Link href="/" className="hover:text-primary">
-//               Home
-//             </Link>
-//             <Link href="/shop" className="hover:text-primary">
-//               Shop
-//             </Link>
-//             <Link href="#" className="hover:text-primary">
-//               About
-//             </Link>
-//             <Link href="/contact" className="hover:text-primary">
-//               Contact
-//             </Link>
-//             <Link href="#" className="hover:text-primary">
-//               Tools
-//             </Link>
-//           </ul>
-//           <div className="flex space-x-12 ml-28">
-//             <Link href="/account">
-//               <User />
-//             </Link>
-//             <Search />
-//             <LoveIcon />
-//             <Link href="/cart">
-//               <AddtoCard />
-//             </Link>
-//           </div>
-//         </div>
-//         {show && (
-//           <div className="w-full bg-white hidden">
-//             <ul className="flex flex-col space-y-4 items-center opacity-80 bg-slate-200">
-//               <li>
-//                 <Link
-//                   href="/"
-//                   className="hover:text-indigo-600 hover:bg-indigo-100"
-//                 >
-//                   Home
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link
-//                   href="/shop"
-//                   className="hover:text-indigo-600 hover:bg-indigo-100"
-//                 >
-//                   Shop
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link
-//                   href="#"
-//                   className="hover:text-indigo-600 hover:bg-indigo-100"
-//                 >
-//                   About
-//                 </Link>
-//               </li>
-//               <li className="hover:text-indigo-600 hover:bg-indigo-100">
-//                 <Link href="#">Contact</Link>
-//               </li>
-//               <li>
-//                 <Link
-//                   href="#"
-//                   className="hover:text-indigo-600 hover:bg-indigo-100"
-//                 >
-//                   Tools
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         )}
-//       </Container>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
